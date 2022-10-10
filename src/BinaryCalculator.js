7f56cf6d3b5d0317a2a2af864579fc220069c2f6
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2022-10-10 21:03:36 +0100

    testing for text content

--- src/BinaryCalculator.js
+++ src/BinaryCalculator.js
@@ -3,10 +3,10 @@ import { ToggleButton } from './Components/ToggleButton';
 export const BinaryCalculator = ({ numberOfButtons }) => {
     return (
         <>
-            <h3>Total is: X</h3>
+            <h3>Total is: 0</h3>
 
             {Array(numberOfButtons).fill().map((_, i) => (
-                <ToggleButton onText="1" offText="0" isOn key={i} />
+                <ToggleButton onText="1" offText="0" key={i} />
             ))}
         </>
     )
--- src/tests/BinaryCalculator.test.js
+++ src/tests/BinaryCalculator.test.js
@@ -6,14 +6,30 @@ test('Displays the correct number of buttons according to the "numberOfButtons p
   render(<BinaryCalculator numberOfButtons={7} />)
 
   let buttons = screen.queryAllByRole('button')
- expect(buttons.length).toEqual(7)
+  expect(buttons.length).toEqual(7)
 
- cleanup()
+  cleanup()
 
- render(<BinaryCalculator numberOfButtons={11} />)
+  render(<BinaryCalculator numberOfButtons={11} />)
 
- buttons = screen.queryAllByRole('button')
-expect(buttons.length).toEqual(11)
+  buttons = screen.queryAllByRole('button')
+  expect(buttons.length).toEqual(11)
+})
 
+test('All buttons start off with text 0', () => {
+  render(<BinaryCalculator numberOfButtons={5} />)
+
+  const buttons = screen.getAllByRole('button')
+
+  buttons.forEach(button => {
+    expect(button).toHaveTextContent('0')
+  })
+})
+
+test('Starting sum is 0', () => {
+  render(<BinaryCalculator numberOfButtons={5} />)
+
+  const sumHeading = screen.getByRole('heading')
+  expect(sumHeading).toHaveTextContent(/total is: 0/i)
 
 })
